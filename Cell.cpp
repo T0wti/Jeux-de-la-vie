9cec60bf8992bde208e7cb0a8aed05bdb48a97ad
T0wti <diego.borto31@gmail.com>
2024-12-03 11:02:30 +0100

    rebase

--- Cell.cpp
+++
@@ -1,8 +0,0 @@
-#include "bibli.h"
-
-//Function to set a Cell in alive state
-void Cell::setAlive(bool state) { alive = state; }
-
-//Function to update a Cell state
-void Cell::update(int neighbors) { alive = alive ? (neighbors == 2 || neighbors == 3) : (neighbors == 3); }
-

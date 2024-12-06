3ca1a186d4a943c60aaac2feed012e177c875f46
Dagmawi229 <dagmawidesta34@gmail.com>
2024-12-06 11:47:46 +0100

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,11 +1,22 @@
 #include "Cell.h"
 
-Cell::Cell(bool state) : isAlive(state) {}
+Cell::Cell(bool state, bool obstacle) : isAlive(state), isObstacle(obstacle) {}
 
 void Cell::setAlive(bool state) {
-    isAlive = state;
+    if (!isObstacle) {
+        isAlive = state;
+    }
+
 }
 
 bool Cell::getState() const {
     return isAlive;
+}
+
+bool Cell::getIsObstacle() const {
+    return isObstacle;
+}
+
+void Cell::setIsObstacle(bool state) {
+    isObstacle = state;
 }

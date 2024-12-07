90790a164dfb6e810e0891e440da901945ee06f3
T0wti <diego.borto31@gmail.com>
2024-12-07 16:40:58 +0100

    main.cpp with english com

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 
 int main() {
     Interface terminal;
-    Grid gameGrid(5,5); // La taille sera déterminée par le fichier
+    Grid gameGrid(5,5); // The size will be set according to the initialization file
     Save saver;
 
     terminal.typeMode();
     terminal.typeGenerations();
     terminal.typeDelay();
-    saver.typeFilePath(); //Fonction pour entrer le chemin d'accès de la grille de départ
+    saver.typeFilePath(); //Function to type the initialization file path
     if (terminal.getMode()==0) {saver.typeSaveFilePath();} //Only ask the path if we are using SFML
     saver.initializeFromFile(gameGrid);
     terminal.redirectMode(gameGrid, saver);
